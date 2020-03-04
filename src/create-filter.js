9a5d3f64691c6ee17fd455063c27d8f99b364169
Oleg S <xwx18@mail.ru>
2020-03-04 23:27:56 +0300

    end second task

---
+++ src/create-filter.js
@@ -0,0 +1,11 @@
+export default (filterInfo) => `<input
+type="radio"
+id="${filterInfo.id}"
+class="filter__input visually-hidden"
+name="filter"
+${filterInfo.checked ? 'checked' : ''}
+${filterInfo.disabled ? 'disabled' : ''}
+>
+<label for="${filterInfo.id}" class="filter__label">
+${filterInfo.title} <span class="${filterInfo.id}-count">${filterInfo.count}</span>
+</label>`;
---
+++ src/mock.js
@@ -0,0 +1,51 @@
+export const filters = [
+  {
+    title: 'ALL',
+    count: '15',
+    checked: true,
+    disabled: false,
+    id: 'filter__all'
+  },
+  {
+    title: 'OVERDUE',
+    count: '0',
+    checked: false,
+    disabled: true,
+    id: 'filter__overdue'
+  },
+  {
+    title: 'TODAY',
+    count: '0',
+    checked: false,
+    disabled: true,
+    id: 'filter__today'
+  },
+  {
+    title: 'FAVORITES',
+    count: '7',
+    checked: false,
+    disabled: false,
+    id: 'filter__favorites'
+  },
+  {
+    title: 'Repeating',
+    count: '2',
+    checked: false,
+    disabled: false,
+    id: 'filter__repeating'
+  },
+  {
+    title: 'Tags',
+    count: '6',
+    checked: false,
+    disabled: false,
+    id: 'filter__tags'
+  },
+  {
+    title: 'ARCHIVE',
+    count: '115',
+    checked: false,
+    disabled: false,
+    id: 'filter__archive'
+  }
+];
